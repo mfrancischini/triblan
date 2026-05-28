20e42d8782b265934db6bd422de0a275c19ba19f
Jose Armando CR <90371472+Chencho34@users.noreply.github.com>
2026-05-28 09:55:19 -0600

    Add vercel adapter to deploy
    * ADD: vercel-adapter-to-deploy
    
    * ADD: vercel-adapter-to-deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercelAdapter from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chencho-dev.vercel.app',
   compressHTML: true,
+  output: 'static',
+  adapter: vercelAdapter(),
   vite: {
     plugins: [tailwindcss()],
     build: {
